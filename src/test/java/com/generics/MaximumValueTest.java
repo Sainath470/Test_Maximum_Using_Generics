c31e7e03497cc69e823bd419feff4702d77ee264
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-10 00:50:42 +0530

    ADDED: Test case to pass by return first position

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -9,15 +9,4 @@ class MaximumValueTest<T> {
         T firstMaxValue= (T) Obj2.findMaximumValue(91,32,43);
         Assertions.assertEquals(91, firstMaxValue);
     }
-    @Test
-    void GivenThreeIntegersGiven_MaximumValue_SecondPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(42, 76, 43);
-        Assertions.assertEquals(76, firstMaxValue);
-    }
-
-    @Test
-    void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(21, 32, 77);
-        Assertions.assertEquals(77, firstMaxValue);
-    }
 }
