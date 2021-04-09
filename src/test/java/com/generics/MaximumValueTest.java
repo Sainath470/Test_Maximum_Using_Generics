4fbc419ae4f4cf4c75750fe75058dbcfa68ee54c
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-10 01:14:20 +0530

    MODIFIED : type to string from float

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -2,22 +2,22 @@ package com.generics;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 class MaximumValueTest<T> {
-    MaximumValue<Float> Obj2= new MaximumValue<>();
+    MaximumValue<String> Obj2= new MaximumValue<>();
 
     @Test
-    void GivenThreeFloatGiven_MaximumValue_FirstPositionReturns(){
-        T firstMaxValue= (T) Obj2.findMaximumValue(91.0,32.3,43.4);
-        Assertions.assertEquals(91.0, firstMaxValue);
+    void Given_ThreeStrings_When_Given_MaximumValue_FirstPositionReturns(){
+        T firstMaxValue= (T) Obj2.findMaximumValue("Peach","Apple","Banana");
+        Assertions.assertEquals("Peach", firstMaxValue);
     }
     @Test
-    void GivenThreeFloatGiven_MaximumValue_SecondPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(42.1, 76.4, 43.3);
-        Assertions.assertEquals(76.4, firstMaxValue);
+    void Given_ThreeStrings_When_Given_MaximumValue_SecondPositionReturns() {
+        T firstMaxValue = (T) Obj2.findMaximumValue("Apple","Kiwi","Banana");
+        Assertions.assertEquals("Kiwi", firstMaxValue);
     }
 
     @Test
-    void GivenThreeFloatGiven_MaximumValue_ThirdPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(21.4, 32.3, 77.5);
-        Assertions.assertEquals(77.5, firstMaxValue);
+    void Given_ThreeStirngs_When_Given_MaximumValue_ThirdPositionReturns() {
+        T firstMaxValue = (T) Obj2.findMaximumValue("Apple","Mango","Peach");
+        Assertions.assertEquals("Peach", firstMaxValue);
     }
 }
