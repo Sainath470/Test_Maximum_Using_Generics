384e5cffde9f5e4ebf1134fe567671c02440727b
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-10 01:01:30 +0530

    MODIFIED : Integer to float and passed 3 float values in test cases

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -2,22 +2,22 @@ package com.generics;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 class MaximumValueTest<T> {
-    MaximumValue<Integer> Obj2= new MaximumValue<>();
+    MaximumValue<Float> Obj2= new MaximumValue<>();
 
     @Test
     void GivenThreeIntegersGiven_MaximumValue_FirstPositionReturns(){
-        T firstMaxValue= (T) Obj2.findMaximumValue(91,32,43);
-        Assertions.assertEquals(91, firstMaxValue);
+        T firstMaxValue= (T) Obj2.findMaximumValue(91.0,32.3,43.4);
+        Assertions.assertEquals(91.0, firstMaxValue);
     }
     @Test
     void GivenThreeIntegersGiven_MaximumValue_SecondPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(42, 76, 43);
-        Assertions.assertEquals(76, firstMaxValue);
+        T firstMaxValue = (T) Obj2.findMaximumValue(42.1, 76.4, 43.3);
+        Assertions.assertEquals(76.4, firstMaxValue);
     }
 
     @Test
     void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(21, 32, 77);
-        Assertions.assertEquals(77, firstMaxValue);
+        T firstMaxValue = (T) Obj2.findMaximumValue(21.4, 32.3, 77.5);
+        Assertions.assertEquals(77.5, firstMaxValue);
     }
 }
