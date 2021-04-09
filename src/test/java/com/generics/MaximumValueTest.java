657f8c903c6033d7bfd5d08e8f369a140930a70a
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-10 01:04:13 +0530

    MODIFIED : Integer to float and passed 3 float values in test cases

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -5,18 +5,18 @@ class MaximumValueTest<T> {
     MaximumValue<Float> Obj2= new MaximumValue<>();
 
     @Test
-    void GivenThreeIntegersGiven_MaximumValue_FirstPositionReturns(){
+    void GivenThreeFloatGiven_MaximumValue_FirstPositionReturns(){
         T firstMaxValue= (T) Obj2.findMaximumValue(91.0,32.3,43.4);
         Assertions.assertEquals(91.0, firstMaxValue);
     }
     @Test
-    void GivenThreeIntegersGiven_MaximumValue_SecondPositionReturns() {
+    void GivenThreeFloatGiven_MaximumValue_SecondPositionReturns() {
         T firstMaxValue = (T) Obj2.findMaximumValue(42.1, 76.4, 43.3);
         Assertions.assertEquals(76.4, firstMaxValue);
     }
 
     @Test
-    void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns() {
+    void GivenThreeFloatGiven_MaximumValue_ThirdPositionReturns() {
         T firstMaxValue = (T) Obj2.findMaximumValue(21.4, 32.3, 77.5);
         Assertions.assertEquals(77.5, firstMaxValue);
     }
