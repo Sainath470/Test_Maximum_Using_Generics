132246b7c7fab80845f38bc16d0e37e9cd10e162
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-11 02:21:51 +0530

    Update MaximumValueTest.java

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -6,18 +6,18 @@ class MaximumValueTest<T> {
 
     @Test
     void GivenThreeIntegersGiven_MaximumValue_FirstPositionReturns(){
-        T firstMaxValue= (T) Obj2.findMaximumValue(91,32,43);
+        T firstMaxValue =  Obj2.findMaximumValue(91,32,43);
         Assertions.assertEquals(91, firstMaxValue);
     }
     @Test
     void GivenThreeIntegersGiven_MaximumValue_SecondPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(42, 76, 43);
-        Assertions.assertEquals(76, firstMaxValue);
+        int secondMaxValue =  Obj2.findMaximumValue(42, 76, 43);
+        Assertions.assertEquals(76, secondMaxValue);
     }
 
     @Test
     void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns() {
-        T firstMaxValue = (T) Obj2.findMaximumValue(21, 32, 77);
-        Assertions.assertEquals(77, firstMaxValue);
+        int thirdMaxValue =  Obj2.findMaximumValue(21, 32, 77);
+        Assertions.assertEquals(77, thirdMaxValue);
     }
 }
