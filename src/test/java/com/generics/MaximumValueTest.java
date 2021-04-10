f805598169bb8fe59c3bb14acd224147b4bb2734
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-11 02:47:37 +0530

    Update MaximumValueTest.java

--- src/test/java/com/generics/MaximumValueTest.java
+++ src/test/java/com/generics/MaximumValueTest.java
@@ -2,21 +2,24 @@ package com.generics;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 class MaximumValueTest<T>{
-
+     
+    MaximumValue<Integer> intObj = new MaximumValue<>();
+    
     @Test
     void GivenThreeIntegersGiven_MaximumValue_FirstPositionReturns(){
-        T firstMaxValue =  Obj2.findMaximumValue(91,32,43);
+        T firstMaxValue = intObj.findMaximumValue(91,32,43);
         Assertions.assertEquals(91, firstMaxValue);
     }
+    
     @Test
     void GivenThreeIntegersGiven_MaximumValue_SecondPositionReturns(){
-        int secondMaxValue =  Obj2.findMaximumValue(42, 76, 43);
+        int secondMaxValue = intObj.findMaximumValue(42, 76, 43);
         Assertions.assertEquals(76, secondMaxValue);
     }
 
     @Test
-    void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns() {
-        int thirdMaxValue = Obj2.findMaximumValue(21, 32, 77);
+    void GivenThreeIntegersGiven_MaximumValue_ThirdPositionReturns(){
+        int thirdMaxValue = intObj.findMaximumValue(21, 32, 77);
         Assertions.assertEquals(77, thirdMaxValue);
     }
 }
